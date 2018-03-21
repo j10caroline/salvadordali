42b7727081d343cdcf0be873fcd66788d57451a4
j10caroline <35707222+j10caroline@users.noreply.github.com>
2018-03-21 15:48:19 -0500

    Add files via upload

---
+++ js/6_script.js
@@ -0,0 +1,77 @@
+  $(document).scroll(function() {
+
+    var y = $(document).scrollTop();
+
+    if (y > 100) {
+      $("#one").fadeIn();
+    }
+
+    else {
+      $("#one").fadeOut();
+    }
+
+    if (y > 200) {
+      $("#two").fadeIn();
+    }
+
+    else {
+      $("#two").fadeOut();
+    }
+
+    if (y > 300) {
+      $("#three").fadeIn();
+    }
+
+    else {
+      $("#three").fadeOut();
+    }
+
+    if (y > 400) {
+      $("#four").fadeIn();
+    }
+
+    else {
+      $("#four").fadeOut();
+    }
+
+    if (y > 500) {
+      $("#five").fadeIn();
+    }
+
+    else {
+      $("#five").fadeOut();
+    }
+
+    if (y > 600) {
+      $("#six").fadeIn();
+    }
+
+    else {
+      $("#six").fadeOut();
+    }
+
+    if (y > 700) {
+      $("#seven").fadeIn();
+    }
+
+    else {
+      $("#seven").fadeOut();
+    }
+
+    if (y > 800) {
+      $("#eight").fadeIn();
+    }
+
+    else {
+      $("#eight").fadeOut();
+    }
+
+    if (y > 900) {
+      $("#nine").fadeIn();
+    }
+
+    else {
+      $("#nine").fadeOut();
+    }
+
+});
